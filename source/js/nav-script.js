1e5ccfb24581fd1b463013b4ba2f4f0ab420f9a1
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-05-26 21:37:13 +0300

    Поповер + логин

--- source/js/nav-script.js
+++ source/js/nav-script.js
@@ -1,3 +1,4 @@
+// Submenu
 let navSubmenu = document.querySelector('.main-nav-submenu');
 let navToggle = document.querySelector('.main-nav-catalog__toggle');
 
@@ -12,5 +13,19 @@ navToggle.addEventListener('click', function () {
     navSubmenu.classList.remove('main-nav-submenu--opened');
     navToggle.classList.add('main-nav-catalog__toggle--closed');
     navToggle.classList.remove('main-nav-catalog__toggle--opened');
+  }
+});
+
+// Popover
+let navPopover = document.querySelector('.popover');
+let navCart = document.querySelector('.main-nav-user__link--cart');
+
+navCart.addEventListener('click', function () {
+  if (navPopover.classList.contains('popover--opened')) {
+    navPopover.classList.remove('popover--opened');
+    navPopover.classList.add('popover--closed');
+  } else {
+    navPopover.classList.remove('popover--closed');
+    navPopover.classList.add('popover--opened');
   }
 });
