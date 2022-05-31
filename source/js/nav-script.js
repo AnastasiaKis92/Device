97459788c8b12f4ca518e1a8c9c078cfe4b414ab
Anastasia Kiseleva <aakkiseleva@gmail.com>
2022-05-31 17:51:35 +0300

    Слайдер + рефакторинг js

--- source/js/nav-script.js
+++ source/js/nav-script.js
@@ -5,13 +5,9 @@ let navToggle = document.querySelector('.main-nav-catalog__toggle');
 navToggle.addEventListener('click', function () {
   if (navSubmenu.classList.contains('main-nav-submenu--closed')) {
     navSubmenu.classList.remove('main-nav-submenu--closed');
-    navSubmenu.classList.add('main-nav-submenu--opened');
-    navToggle.classList.remove('main-nav-catalog__toggle--closed');
     navToggle.classList.add('main-nav-catalog__toggle--opened');
   } else {
     navSubmenu.classList.add('main-nav-submenu--closed');
-    navSubmenu.classList.remove('main-nav-submenu--opened');
-    navToggle.classList.add('main-nav-catalog__toggle--closed');
     navToggle.classList.remove('main-nav-catalog__toggle--opened');
   }
 });
@@ -21,11 +17,9 @@ let navPopover = document.querySelector('.popover');
 let navCart = document.querySelector('.main-nav-user__link--cart');
 
 navCart.addEventListener('click', function () {
-  if (navPopover.classList.contains('popover--opened')) {
-    navPopover.classList.remove('popover--opened');
-    navPopover.classList.add('popover--closed');
-  } else {
+  if (navPopover.classList.contains('popover--closed')) {
     navPopover.classList.remove('popover--closed');
-    navPopover.classList.add('popover--opened');
+  } else {
+    navPopover.classList.add('popover--closed');
   }
 });
